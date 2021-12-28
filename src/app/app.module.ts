bb390cccbd5bc44c44d301d38c931e267fbc9a7c
sqrmyy <dn41.ra20adtdjw@gmail.com>
2021-12-28 17:37:05 +0900

    作ったコンポーネントをアプリに組み込んでみる

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule
